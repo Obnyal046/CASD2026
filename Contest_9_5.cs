6429f7fa1a635de0103681e534319deacf3f8b8b
Obnyal046 <djpioneerx5@gmail.com>
2026-03-09 21:27:45 +0300

    Contest_9_5.cs

---
+++ Contest_9_5.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        int n = s.Length;
+        int[] pi = new int[n];
+        for (int i = 1; i < n; i++)
+        {
+            int j = pi[i - 1];
+            while (j > 0 && s[i] != s[j])
+                j = pi[j - 1];
+            if (s[i] == s[j])
+                j++;
+            pi[i] = j;
+        }
+        int period = n - pi[n - 1];
+        if (n % period == 0)
+            Console.WriteLine(period);
+        else
+            Console.WriteLine(n);
+    }
+}
