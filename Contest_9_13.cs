0eef15edcb19381e0ba253d16b91383d36b9d20d
Obnyal046 <djpioneerx5@gmail.com>
2026-03-18 10:11:49 +0300

    Contest_9_13.cs

---
+++ Contest_9_13.cs
@@ -0,0 +1,135 @@
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        string t = Console.ReadLine();
+        string combined = s + "#" + t;
+        int[] sa = BuildSuffixArray(combined);
+        int[] lcp = BuildLCP(combined, sa);
+        int ls = s.Length;
+        int bestLen = 0;
+        int bestStart = -1;
+
+        for (int i = 1; i < combined.Length; i++)
+        {
+            int pos1 = sa[i - 1];
+            int pos2 = sa[i];
+            bool inS1 = pos1 < ls;
+            bool inT1 = pos1 > ls;
+            bool inS2 = pos2 < ls;
+            bool inT2 = pos2 > ls;
+
+            if ((inS1 && inT2) || (inT1 && inS2))
+            {
+                int len = lcp[i - 1];
+                if (len > bestLen)
+                {
+                    bestLen = len;
+                    bestStart = inS1 ? pos1 : pos2;
+                }
+            }
+        }
+
+        if (bestLen == 0)
+            Console.WriteLine();
+        else
+            Console.WriteLine(s.Substring(bestStart, bestLen));
+    }
+
+    static int[] BuildSuffixArray(string s)
+    {
+        int n = s.Length;
+        int[] sa = new int[n];
+        int[] rank = new int[n];
+        int[] tmp = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            sa[i] = i;
+            rank[i] = s[i];
+        }
+        for (int k = 1; k < n; k <<= 1)
+        {
+            int[] second = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                int pos = sa[i] + k;
+                second[i] = pos < n ? rank[pos] + 1 : 0;
+            }
+            int maxSecond = 0;
+            for (int i = 0; i < n; i++)
+                if (second[i] > maxSecond) maxSecond = second[i];
+            int[] cnt = new int[maxSecond + 2];
+            for (int i = 0; i < n; i++)
+                cnt[second[i]]++;
+            for (int i = 1; i < cnt.Length; i++)
+                cnt[i] += cnt[i - 1];
+            int[] newSa = new int[n];
+            for (int i = n - 1; i >= 0; i--)
+            {
+                int idx = --cnt[second[i]];
+                newSa[idx] = sa[i];
+            }
+            int[] first = new int[n];
+            for (int i = 0; i < n; i++)
+                first[i] = rank[newSa[i]] + 1;
+            int maxFirst = 0;
+            for (int i = 0; i < n; i++)
+                if (first[i] > maxFirst) maxFirst = first[i];
+            cnt = new int[maxFirst + 2];
+            for (int i = 0; i < n; i++)
+                cnt[first[i]]++;
+            for (int i = 1; i < cnt.Length; i++)
+                cnt[i] += cnt[i - 1];
+            int[] newSa2 = new int[n];
+            for (int i = n - 1; i >= 0; i--)
+            {
+                int idx = --cnt[first[i]];
+                newSa2[idx] = newSa[i];
+            }
+            sa = newSa2;
+            tmp[sa[0]] = 0;
+            for (int i = 1; i < n; i++)
+            {
+                int cur1 = rank[sa[i]];
+                int cur2 = (sa[i] + k < n) ? rank[sa[i] + k] : -1;
+                int prev1 = rank[sa[i - 1]];
+                int prev2 = (sa[i - 1] + k < n) ? rank[sa[i - 1] + k] : -1;
+                if (cur1 == prev1 && cur2 == prev2)
+                    tmp[sa[i]] = tmp[sa[i - 1]];
+                else
+                    tmp[sa[i]] = tmp[sa[i - 1]] + 1;
+            }
+            Array.Copy(tmp, rank, n);
+            if (rank[sa[n - 1]] == n - 1) break;
+        }
+        return sa;
+    }
+
+    static int[] BuildLCP(string s, int[] sa)
+    {
+        int n = s.Length;
+        int[] rank = new int[n];
+        for (int i = 0; i < n; i++)
+            rank[sa[i]] = i;
+        int[] lcp = new int[n - 1];
+        int h = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (rank[i] == n - 1)
+            {
+                h = 0;
+                continue;
+            }
+            int j = sa[rank[i] + 1];
+            while (i + h < n && j + h < n && s[i + h] == s[j + h])
+                h++;
+            lcp[rank[i]] = h;
+            if (h > 0) h--;
+        }
+        return lcp;
+    }
+}
